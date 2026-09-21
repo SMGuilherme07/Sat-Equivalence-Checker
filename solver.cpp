bcee8373c338f2d0126ac10a7aaa1c8990cfa1d2
SMGuilherme07 <guimsoares_ita@hotmail.com>
2026-09-20 22:13:36 -0300

    add .cnf's

--- solver.cpp
+++ solver.cpp
@@ -5,9 +5,16 @@
 
 using namespace std;
 
-enum Cat { satisfied, unsatisfied, normal, completed };
+enum Cat
+{
+  satisfied,
+  unsatisfied,
+  normal,
+  completed
+};
 
-class Formula {
+class Formula
+{
 public:
   int num_vars;
   vector<int> literals;
@@ -15,11 +22,13 @@ public:
   vector<int> literal_polarity;
   vector<vector<int>> clauses;
 
-  Formula() {
-      num_vars = 0;
+  Formula()
+  {
+    num_vars = 0;
   }
 
-  Formula(const Formula &f) {
+  Formula(const Formula &f)
+  {
     num_vars = f.num_vars;
     literals = f.literals;
     clauses = f.clauses;
@@ -27,31 +36,39 @@ public:
     literal_polarity = f.literal_polarity;
   }
 
-  void recalculate_stats() {
+  void recalculate_stats()
+  {
     literal_frequency.assign(num_vars, 0);
     literal_polarity.assign(num_vars, 0);
     literals.assign(num_vars, -1);
 
-    for (int i = 0; i < clauses.size(); i++) {
-      for (int j = 0; j < clauses[i].size(); j++) {
+    for (int i = 0; i < clauses.size(); i++)
+    {
+      for (int j = 0; j < clauses[i].size(); j++)
+      {
         int lit = clauses[i][j];
         int var = lit / 2;
-        
+
         literal_frequency[var]++;
-        
-        if (lit % 2 == 0) {
-            literal_polarity[var]++;
-        } else {
-            literal_polarity[var]--;
+
+        if (lit % 2 == 0)
+        {
+          literal_polarity[var]++;
+        }
+        else
+        {
+          literal_polarity[var]--;
         }
       }
     }
   }
 };
 
-Formula readCNF(string filename) {
+Formula readCNF(string filename)
+{
   ifstream file(filename);
-  if (!file.is_open()) {
+  if (!file.is_open())
+  {
     cout << "Erro ao abrir: " << filename << endl;
     exit(1);
   }
@@ -61,11 +78,14 @@ Formula readCNF(string filename) {
   string s;
   int clause_count;
 
-  while (file >> c) {
-    if (c == 'c') {
-        getline(file, s);
+  while (file >> c)
+  {
+    if (c == 'c')
+    {
+      getline(file, s);
     }
-    else if (c == 'p') {
+    else if (c == 'p')
+    {
       file >> s;
       file >> formula.num_vars;
       file >> clause_count;
@@ -76,22 +96,28 @@ Formula readCNF(string filename) {
   formula.clauses.resize(clause_count);
   int literal;
 
-  for (int i = 0; i < clause_count; i++) {
-    while (file >> literal && literal != 0) {
-      if (literal > 0) {
+  for (int i = 0; i < clause_count; i++)
+  {
+    while (file >> literal && literal != 0)
+    {
+      if (literal > 0)
+      {
         formula.clauses[i].push_back(2 * (literal - 1));
-      } else {
+      }
+      else
+      {
         formula.clauses[i].push_back(2 * ((-1) * literal - 1) + 1);
       }
     }
   }
-  
+
   file.close();
   formula.recalculate_stats();
   return formula;
 }
 
-class SATSolverDPLL {
+class SATSolverDPLL
+{
 private:
   int unit_propagate(Formula &f);
   int DPLL(Formula f);
@@ -101,27 +127,35 @@ public:
   bool is_sat(Formula f);
 };
 
-int SATSolverDPLL::unit_propagate(Formula &f) {
+int SATSolverDPLL::unit_propagate(Formula &f)
+{
   bool unit_clause_found;
-  if (f.clauses.size() == 0) {
-      return Cat::satisfied;
+  if (f.clauses.size() == 0)
+  {
+    return Cat::satisfied;
   }
 
-  do {
+  do
+  {
     unit_clause_found = false;
-    for (int i = 0; i < f.clauses.size(); i++) {
-      if (f.clauses[i].size() == 1) {
+    for (int i = 0; i < f.clauses.size(); i++)
+    {
+      if (f.clauses[i].size() == 1)
+      {
         unit_clause_found = true;
         int lit = f.clauses[i][0];
         f.literals[lit / 2] = lit % 2;
         f.literal_frequency[lit / 2] = -1;
 
         int result = apply_transform(f, lit / 2);
-        if (result == Cat::satisfied || result == Cat::unsatisfied) {
-            return result;
+        if (result == Cat::satisfied || result == Cat::unsatisfied)
+        {
+          return result;
         }
         break;
-      } else if (f.clauses[i].size() == 0) {
+      }
+      else if (f.clauses[i].size() == 0)
+      {
         return Cat::unsatisfied;
       }
     }
@@ -130,23 +164,31 @@ int SATSolverDPLL::unit_propagate(Formula &f) {
   return Cat::normal;
 }
 
-int SATSolverDPLL::apply_transform(Formula &f, int literal_to_apply) {
+int SATSolverDPLL::apply_transform(Formula &f, int literal_to_apply)
+{
   int value_to_apply = f.literals[literal_to_apply];
 
-  for (int i = 0; i < f.clauses.size(); i++) {
-    for (int j = 0; j < f.clauses[i].size(); j++) {
-      if ((2 * literal_to_apply + value_to_apply) == f.clauses[i][j]) {
+  for (int i = 0; i < f.clauses.size(); i++)
+  {
+    for (int j = 0; j < f.clauses[i].size(); j++)
+    {
+      if ((2 * literal_to_apply + value_to_apply) == f.clauses[i][j])
+      {
         f.clauses.erase(f.clauses.begin() + i);
         i--;
-        if (f.clauses.size() == 0) {
-            return Cat::satisfied;
+        if (f.clauses.size() == 0)
+        {
+          return Cat::satisfied;
         }
         break;
-      } else if (f.clauses[i][j] / 2 == literal_to_apply) {
+      }
+      else if (f.clauses[i][j] / 2 == literal_to_apply)
+      {
         f.clauses[i].erase(f.clauses[i].begin() + j);
         j--;
-        if (f.clauses[i].size() == 0) {
-            return Cat::unsatisfied;
+        if (f.clauses[i].size() == 0)
+        {
+          return Cat::unsatisfied;
         }
         break;
       }
@@ -155,99 +197,122 @@ int SATSolverDPLL::apply_transform(Formula &f, int literal_to_apply) {
   return Cat::normal;
 }
 
-int SATSolverDPLL::DPLL(Formula f) {
+int SATSolverDPLL::DPLL(Formula f)
+{
   int result = unit_propagate(f);
-  if (result == Cat::satisfied) return Cat::satisfied;
-  if (result == Cat::unsatisfied) return Cat::unsatisfied;
+  if (result == Cat::satisfied)
+    return Cat::satisfied;
+  if (result == Cat::unsatisfied)
+    return Cat::unsatisfied;
 
-  // Encontrando a variavel que mais apareceu
   int max_freq = -1;
   int variavel_escolhida = 0;
 
-  for (int k = 0; k < f.literal_frequency.size(); k++) {
-      if (f.literal_frequency[k] > max_freq) {
-          max_freq = f.literal_frequency[k];
-          variavel_escolhida = k;
-      }
+  for (int k = 0; k < f.literal_frequency.size(); k++)
+  {
+    if (f.literal_frequency[k] > max_freq)
+    {
+      max_freq = f.literal_frequency[k];
+      variavel_escolhida = k;
+    }
   }
 
-  for (int j = 0; j < 2; j++) {
+  for (int j = 0; j < 2; j++)
+  {
     Formula new_f = f;
-    
-    if (new_f.literal_polarity[variavel_escolhida] > 0) {
-        new_f.literals[variavel_escolhida] = j;
-    } else {
-        new_f.literals[variavel_escolhida] = (j + 1) % 2;
+
+    if (new_f.literal_polarity[variavel_escolhida] > 0)
+    {
+      new_f.literals[variavel_escolhida] = j;
     }
-    
+    else
+    {
+      new_f.literals[variavel_escolhida] = (j + 1) % 2;
+    }
+
     new_f.literal_frequency[variavel_escolhida] = -1;
 
     int transform_result = apply_transform(new_f, variavel_escolhida);
-    if (transform_result == Cat::satisfied) return Cat::satisfied;
-    if (transform_result == Cat::unsatisfied) continue;
+    if (transform_result == Cat::satisfied)
+      return Cat::satisfied;
+    if (transform_result == Cat::unsatisfied)
+      continue;
 
-    if (DPLL(new_f) == Cat::satisfied) return Cat::satisfied;
+    if (DPLL(new_f) == Cat::satisfied)
+      return Cat::satisfied;
   }
-  
+
   return Cat::unsatisfied;
 }
 
-bool SATSolverDPLL::is_sat(Formula f) {
+bool SATSolverDPLL::is_sat(Formula f)
+{
   return DPLL(f) == Cat::satisfied;
 }
 
-bool check_difference(Formula F_base, vector<int> C_alvo, SATSolverDPLL &solver) {
-  for (int i = 0; i < C_alvo.size(); i++) {
+bool check_difference(Formula F_base, vector<int> C_alvo, SATSolverDPLL &solver)
+{
+  for (int i = 0; i < C_alvo.size(); i++)
+  {
     int lit = C_alvo[i];
     int negated_lit;
 
-    //Inverte o sinal do literal
-    if (lit % 2 == 0) {
-        negated_lit = lit + 1;
-    } else {
-        negated_lit = lit - 1;
+    if (lit % 2 == 0)
+    {
+      negated_lit = lit + 1;
+    }
+    else
+    {
+      negated_lit = lit - 1;
     }
 
-    //Cria um vetor temporário só pra guardar esse literal sozinho
     vector<int> clausula_unitaria;
     clausula_unitaria.push_back(negated_lit);
     F_base.clauses.push_back(clausula_unitaria);
   }
-  
+
   F_base.recalculate_stats();
   return solver.is_sat(F_base);
 }
 
-bool check_equivalence(Formula f1, Formula f2, SATSolverDPLL &solver) {
-  // Ajusta a quantidade de variaveis pegando a maior
+bool check_equivalence(Formula f1, Formula f2, SATSolverDPLL &solver)
+{
+
   int max_vars = f1.num_vars;
-  if (f2.num_vars > max_vars) {
-      max_vars = f2.num_vars;
+  if (f2.num_vars > max_vars)
+  {
+    max_vars = f2.num_vars;
   }
   f1.num_vars = max_vars;
   f2.num_vars = max_vars;
 
   // Testa F1 contra F2
-  for (int i = 0; i < f2.clauses.size(); i++) {
-    if (check_difference(f1, f2.clauses[i], solver) == true) {
-        return false; 
+  for (int i = 0; i < f2.clauses.size(); i++)
+  {
+    if (check_difference(f1, f2.clauses[i], solver) == true)
+    {
+      return false;
     }
   }
 
   // Testa F2 contra F1
-  for (int i = 0; i < f1.clauses.size(); i++) {
-    if (check_difference(f2, f1.clauses[i], solver) == true) {
-        return false;
+  for (int i = 0; i < f1.clauses.size(); i++)
+  {
+    if (check_difference(f2, f1.clauses[i], solver) == true)
+    {
+      return false;
     }
   }
 
   return true;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
   vector<string> argumentos;
-  for (int i = 0; i < argc; i++) {
-      argumentos.push_back(argv[i]);
+  for (int i = 0; i < argc; i++)
+  {
+    argumentos.push_back(argv[i]);
   }
 
   string file1 = argumentos[1];
@@ -258,9 +323,12 @@ int main(int argc, char* argv[]) {
 
   SATSolverDPLL solver;
 
-  if (check_equivalence(f1, f2, solver) == true) {
+  if (check_equivalence(f1, f2, solver) == true)
+  {
     cout << "Iguais (Equivalentes)" << endl;
-  } else {
+  }
+  else
+  {
     cout << "Diferentes" << endl;
   }
 
